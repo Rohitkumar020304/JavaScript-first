deefac7c0d26ceb21b40d0abca5bdfefb1ca51fb
Rohitkumar020304 <145589790+Rohitkumar020304@users.noreply.github.com>
2024-06-15 18:17:53 +0000

    var/dataTypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 620726
+let accountEmail = "kumarrohit@google.com"
+var accountPassword = "google_123"
+accountCity = "Bengaluru"
+let accountAge;
+/*
+this is comment paragraph 
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountAge])
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict";
+let name = "Rohit"
+let age = 20
+let isLoggedIn = false
+let temperature = null
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof temperature)
