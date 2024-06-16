b64c14fa78cb9068247b191719496ad26f0e2a4c
Rohitkumar020304 <145589790+Rohitkumar020304@users.noreply.github.com>
2024-06-16 19:47:25 +0000

    datatypes-summary

---
+++ 01_basic/datatypes-summary.js
@@ -0,0 +1,14 @@
+let Rohit = 45
+let kohli = Rohit
+kohli = 18
+// console.log(Rohit)
+// console.log(kohli)
+
+let userOne = {
+    email: "rohit@google.com",
+    upi: "rohit@ybl"
+}
+let userTwo = userOne
+userTwo.email = "kumar@google.com"
+console.log(userOne.email)
+console.log(userTwo.email)
