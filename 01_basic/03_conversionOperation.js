b5bf9009fc2cdb9d8242cea09b5f175f8e9128c4
Rohitkumar020304 <145589790+Rohitkumar020304@users.noreply.github.com>
2024-06-16 18:41:16 +0000

    Conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,10 @@
+let age = 21
+let name = "Rohit"
+let stringAge = String(age)
+let boolAge = Boolean(age)
+let numAge = Number(boolAge)
+// console.log(typeof numAge);
+// console.log(numAge); => 1
+let numName = Number(name)
+console.log(typeof numName);
+console.log(numName);/* => Nan */
